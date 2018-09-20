7422c92969faa275c34d080ab01102b5b75b56a8
fniroui <farzad.niroui@gmail.com>
2018-09-19 19:30:45 -0700

    Using TensorBoard

--- scripts/train_g1_320.sh
+++ scripts/train_g1_320.sh
@@ -1,3 +1,4 @@
 python vid2vid/train.py --name depth2room_320 --dataroot datasets/sceneNet --input_nc 1 \
 --loadSize 320 --n_frames_total 8 --niter_step 2 --niter_fix_global 8 --niter_decay 5 --num_D 1 \
---load_pretrain checkpoints/label2city_512 --n_downsample_G 2 --max_frames_per_gpu 2 --n_scales_spatial 2 
+--load_pretrain checkpoints/label2city_512 --n_downsample_G 2 --max_frames_per_gpu 2 \
+--n_scales_spatial 2 --tf_log

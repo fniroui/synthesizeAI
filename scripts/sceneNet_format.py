23ece833899373b93f416bacc46cf23a9dfe2f18
fniroui <farzad.niroui@gmail.com>
2018-09-28 14:52:27 -0700

    Removed depth image conversion.

--- scripts/sceneNet_format.py
+++ scripts/sceneNet_format.py
@@ -8,6 +8,9 @@ from PIL import Image
 parser = argparse.ArgumentParser(description='format')
 parser.add_argument('--dir', type=str, default= '~/dataset/0/', help='location where sceneNet dataset is extracted from home directory')
 parser.add_argument('--keep',  action='store_true', help='Call to keep copy images instead of moving them')
+parser.add_argument('--color',  action='store_true', help='Do not convert to black & white')
+parser.add_argument('--seq_num',  type=int, default=100, help='Number of sequences to process')
+
 
 def _sort_dir(dir_list):
     dir_list.sort(key=lambda f: int(f.rsplit(os.path.extsep, 1)[0].rsplit(None,1)[-1]))
@@ -45,7 +48,6 @@ if __name__ == '__main__':
         for _, _, fnames in sorted(os.walk(in_dir_A)):
             data_num = 0
             fnames = _sort_dir(fnames)
-            # fnames.sort(key=lambda f: int(f.rsplit(os.path.extsep, 1)[0].rsplit(None,1)[-1]))
 
             for fname in fnames:
                 in_depth_name = os.path.join(in_dir_A, fname)
@@ -54,19 +56,23 @@ if __name__ == '__main__':
                 out_depth_name = os.path.join(out_dir_A, str(seq_num).zfill(4) + '_' + str(data_num).zfill(4) + '.png')
                 out_rgb_name = os.path.join(out_dir_B, str(seq_num).zfill(4) + '_' + str(data_num).zfill(4) + '.jpg')
 
-                img_depth = Image.open(in_depth_name)
-                img_depth = np.array(img_depth, dtype=np.uint16)
-                img_depth = np.divide(img_depth, 100.0)
-                img_depth = img_depth.astype(np.uint8)
-                img_depth = Image.fromarray(img_depth, 'P')
-                img_depth.save(out_depth_name)
-    
                 if args.keep:
-                    copyfile(in_rgb_name, out_rgb_name)
+                    copyfile(in_depth_name, out_depth_name)
+                    if args.color:
+                        copyfile(in_rgb_name, out_rgb_name)
+                    else:
+                        img = Image.open(in_rgb_name).convert('L')
+                        img.save(out_rgb_name)
                 else:
-                    os.remove(in_depth_name)
-                    os.rename(in_rgb_name, out_rgb_name)
+                    os.rename(in_depth_name, out_depth_name)
+                    if args.color:
+                        os.rename(in_rgb_name, out_rgb_name)
+                    else:
+                        img = Image.open(in_rgb_name).convert('L')
+                        img.save(out_rgb_name)
+                        os.remove(in_rgb_name)
 
                 data_num += 1
-            
-        seq_num += 1
+
+        seq_num += 1 
+        if seq_num >= args.seq_num: break 
